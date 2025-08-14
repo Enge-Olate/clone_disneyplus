0680cab5b944cd3b54270a57e5d938b3369be302
Enge-Olate <marcioalexisolate@live.com>
2025-08-14 11:01:55 -0300

    Alterando gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,29 @@
-//imports
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-import gulp from 'gulp';
-import gulpSass from 'gulp-sass';
-import * as sass from 'sass';
-import GulpUglify from 'gulp-uglify';
-import imagemin, { gifsicle, mozjpeg, optipng, svgo } from 'gulp-imagemin';
-//fim imports
-
-//compila sass
-const compila_sass = () => {
-    return gulp
-        .src("./src/styles/*.scss")
-        .pipe(gulpSass(sass)({ outputStyle: "compressed" }))
-        .pipe(gulp.dest("./dist/styles/"));
-};
-//fim compila sass
-
-// comprime imagens
-const comprime_imagens = () => {
-    return gulp.src('./src/imagens/**/*')
-        .pipe(imagemin([
-            gifsicle({optimizationLevel: true}),
-            mozjpeg({quality: 75, progressive: true}),
-            optipng({optimizationLevel: 5}),
-            svgo({plugins:[{name:'removeViewBox', active:true}, {name:'cleanupIds',active:true}]})
-        ]))
-        .pipe(gulp.dest('./dist/imagem_min'));
-};
-
-// comprime js
-const comprime_js = ()=>{
+function scripts() {
     return gulp.src('./src/scripts/*.js')
-        .pipe(GulpUglify())
-        .pipe(gulp.dest('./dist/script_min/'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
 }
 
-//fim comprime js
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
 
-//fim comprime imagens
-// exports padrão
-export default gulp.parallel(comprime_imagens, compila_sass, comprime_js);
-// fim 
-// observando
-export const watch = () => {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(compila_sass));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(comprime_js));
-};
-// fim observando
+function images() {
+    return gulp.src('./src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
